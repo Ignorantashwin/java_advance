a7e8c942e90e7258e50908246faf7cba50032cb7
Ashwin <ashwinhuyrr@gmail.com>
2026-08-08 15:49:20 +0530

    Done

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Print the greeting below.
       Scanner sc = new Scanner(System.in);
-     String s = sc.nextLine();
-     System.out.println(s.toUpperCase());
+     String name = sc.nextLine();
+     int age = Integer.parseInt(sc.nextLine());
+     String result = "Hi, %s! You are %d years old.".formatted(name, age);
+     // prints same 
+     //String result1 = String.format("Hi, %s! You are %d years old.", name, age);
+     System.out.println(result);
+
+     //System.out.println(result1);
 
     }
 }
