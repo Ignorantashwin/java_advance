dc569d125150939d9f5e50ffdd47eb8596d6d0fa
Ashwin <ashwinhuyrr@gmail.com>
2026-08-08 18:34:51 +0530

    Done

--- Main.java
+++ Main.java
@@ -5,11 +5,23 @@ public class Main {
         // Print the greeting below.
     
       Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(square(n));
+       int x1 = sc.nextInt();
+    int x2 = sc.nextInt();
+    int y1 = sc.nextInt();
+    int y2 = sc.nextInt();
+    System.out.println(sumSquare(x1, x2, y1, y2));
        
-    }
-    static int square(int n){
-        return n*n;
+}
+static int sumSquare(int a1 , int a2, int b1, int b2){
+     int totalSum = 0;
+     if (a1 > b1 && a2 > b2) {
+        totalSum = (a1*a1) - (b1*b1) + (a2*a2) - (b2*b2);
+        return totalSum;
+     }
+     else if(a1 < b1 && a2 < b2){
+     totalSum =   (b1*b1)- (a1*a1) +  (b2*b2)- (a2*a2);
+     }
+     
+        return totalSum;
     }
 }
---
+++ tests/09-classes/lesson9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lesson9 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x1 = sc.nextInt();
+    int x2 = sc.nextInt();
+    int y1 = sc.nextInt();
+    int y2 = sc.nextInt();
+    System.out.println(sumSquare(x1, x2, y1, y2));
+    }
+    static int sumSquare(int a1 , int a2, int b1, int b2){
+     int totalSum = 0;
+     totalSum = ((a1*a1)-(a2*a2)) - ((b1*b1)- (b2*b2));
+        return totalSum;
+    }
+}
