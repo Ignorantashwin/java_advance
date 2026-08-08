e9173b3ac048aea87b984452e962e9515260dd3f
Ignorantashwin <ashwinhuyrr@gmail.com>
2026-08-08 19:02:37 +0530

    Update lesson9.java

--- tests/09-classes/lesson9.java
+++ tests/09-classes/lesson9.java
@@ -11,15 +11,7 @@ public class lesson9 {
        
 }
 static int sumSquare(int a1 , int a2, int b1, int b2){
-     int totalSum = 0;
-     if (a1 > b1 && a2 > b2) {
-        totalSum = (a1*a1) - (b1*b1) + (a2*a2) - (b2*b2);
-        return totalSum;
-     }
-     else {
-     totalSum =   (b1*b1)- (a1*a1) +  (b2*b2)- (a2*a2);
-     }
+ return Math.abs( (a1*a1) - (b1*b1) ) + Math.abs( (a2*a2) - (b2*b2) );
      
-        return totalSum;
     }
 }
