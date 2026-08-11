6d1daa291dfbe9410910b451018f80f9cf1433cf
Ashwin <ashwinhuyrr@gmail.com>
2026-08-11 11:10:37 +0530

    Lesson 9 classes done

--- Main.java
+++ Main.java
@@ -6,22 +6,16 @@ public class Main {
     
       Scanner sc = new Scanner(System.in);
        int x1 = sc.nextInt();
-    int x2 = sc.nextInt();
     int y1 = sc.nextInt();
+    int x2 = sc.nextInt();
     int y2 = sc.nextInt();
-    System.out.println(sumSquare(x1, x2, y1, y2));
+    System.out.println(square(x1, y1, x2, y2));
        
 }
-static int sumSquare(int a1 , int a2, int b1, int b2){
-     int totalSum = 0;
-     if (a1 > b1 && a2 > b2) {
-        totalSum = (a1*a1) - (b1*b1) + (a2*a2) - (b2*b2);
-        return totalSum;
-     }
-     else {
-     totalSum =   (b1*b1)- (a1*a1) +  (b2*b2)- (a2*a2);
-     }
-     
-        return totalSum;
-    }
+static int square(int x1, int y1, int x2, int y2){
+    int dx = x2-x1;
+    int dy = y2-y1;
+    int sum = dx*dx + dy*dy;
+    return sum;
+}
 }
---
+++ tests/09-classes/lesson9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class lesson9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1= sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+       System.out.println(square(x1, y1, x2, y2));
+    }
+    static int square(int x1, int y1, int x2, int y2){
+        int dx = x2-x1;
+        int dy = y2-y1;
+        int sum = dx*dx + dy*dy;
+        return sum;
+    }
+}
