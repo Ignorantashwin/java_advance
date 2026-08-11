0470dd3e0edc3fe06c14b58f3fa819fc331171a8
Ashwin <ashwinhuyrr@gmail.com>
2026-08-11 12:31:59 +0530

    Done chapter 10

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -5,17 +7,16 @@ public class Main {
         // Print the greeting below.
     
       Scanner sc = new Scanner(System.in);
-       int x1 = sc.nextInt();
-    int y1 = sc.nextInt();
-    int x2 = sc.nextInt();
-    int y2 = sc.nextInt();
-    System.out.println(square(x1, y1, x2, y2));
+     String line = sc.nextLine();
+     String[] arr = line.split(" ");
+     int max = Integer.parseInt(arr[0]);
+     for (int i = 1; i < arr.length; i++) {
+        int v = Integer.parseInt(arr[i]);
+        if (max < v) {
+            max = v;
+        }
+     }
+ System.out.println(max);
        
-}
-static int square(int x1, int y1, int x2, int y2){
-    int dx = x2-x1;
-    int dy = y2-y1;
-    int sum = dx*dx + dy*dy;
-    return sum;
 }
 }
